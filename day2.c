67f1edeb5ca448b5c688b9e19c899f175f3af0f8
Garv Bhatia <bhatiagarv07@gmail.com>
2026-02-10 00:00:28 +0530

    Create day2.c

---
+++ day2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <limits.h>
+
+int maxProfit(int* prices, int pricesSize) {
+    int minPrice = INT_MAX;
+    int maxProfit = 0;
+
+    for (int i = 0; i < pricesSize; i++) {
+        if (prices[i] < minPrice)
+            minPrice = prices[i];
+        if (prices[i] - minPrice > maxProfit)
+            maxProfit = prices[i] - minPrice;
+    }
+
+    return maxProfit;
+}
