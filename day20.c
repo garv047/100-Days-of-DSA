77dc19821fb136d73a5f3fd94fc53761ff76ca6a
Garv Bhatia <bhatiagarv07@gmail.com>
2026-04-08 20:59:46 +0530

    find triplets
    Fix memory allocation for returnColumnSizes and update comments.

--- day20.c
+++ day20.c
@@ -1,15 +1,12 @@
 #include <stdlib.h>
 
-// Comparator for qsort
 int compare(const void* a, const void* b) {
     return ((int)a - (int)b);
 }
 
 int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
-    // Sort the array
     qsort(nums, numsSize, sizeof(int), compare);
 
-    // Allocate maximum possible space
     int capacity = 10000;
     int** result = (int*)malloc(capacity * sizeof(int));
     returnColumnSizes = (int)malloc(capacity * sizeof(int));
@@ -17,7 +14,6 @@ int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes
     returnSize = 0;
 
     for (int i = 0; i < numsSize - 2; i++) {
-        // Skip duplicate first elements
         if (i > 0 && nums[i] == nums[i - 1])
             continue;
 
@@ -28,7 +24,6 @@ int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes
             int sum = nums[i] + nums[left] + nums[right];
 
             if (sum == 0) {
-                // Store the triplet
                 result[*returnSize] = (int)malloc(3 * sizeof(int));
                 result[*returnSize][0] = nums[i];
                 result[*returnSize][1] = nums[left];
@@ -37,7 +32,6 @@ int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes
                 (*returnColumnSizes)[*returnSize] = 3;
                 (*returnSize)++;
 
-                // Skip duplicates
                 while (left < right && nums[left] == nums[left + 1]) left++;
                 while (left < right && nums[right] == nums[right - 1]) right--;
 
