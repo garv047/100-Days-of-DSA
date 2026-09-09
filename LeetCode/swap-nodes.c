8b1717cafdafb3b2508c610ed0b3d2bbb27d43eb
Garv Bhatia <bhatiagarv07@gmail.com>
2026-09-09 20:28:27 +0530

    Swap adjacent nodes in list

---
+++ LeetCode/swap-nodes.c
@@ -0,0 +1,23 @@
+struct ListNode* swapPairs(struct ListNode* head)
+{
+    struct ListNode dummy;
+    dummy.next = head;
+
+    struct ListNode* prev = &dummy;
+
+    while (prev->next != NULL && prev->next->next != NULL)
+    {
+        struct ListNode* first = prev->next;
+        struct ListNode* second = first->next;
+
+        // Swap the two nodes
+        first->next = second->next;
+        second->next = first;
+        prev->next = second;
+
+        // Move to the next pair
+        prev = first;
+    }
+
+    return dummy.next;
+}
